9910f8f8c99b347dcf4ad5e0c4eafc4f36672849
Leonardo Gomiero <leoleonardo2000@gmail.com>
2024-01-27 00:34:55 -0300

    Email form concluded

--- scripts/email.js
+++ scripts/email.js
@@ -1,16 +1,12 @@
 function sendEmail() {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "leoleonardo2000@gmail.com",
-        Password: "30F512E0DB7A650EB25499687F7407145866",
-        To: 'leonardo.gomiero.c.c@gmail.com',
-        From: "you@isp.com",
-        Subject: "This is the subject",
-        Body: "Name: " + document.getElementById("name").value +
-            "<br> Email: " + document.getElementById("email").value +
-            "<br> Subject: " + document.getElementById("subject").value +
-            "<br> Message: " + document.getElementById("message").value
-    }).then(
-        message => alert("Message Sent Successfully")
-    );
+    
+    let parms ={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_f67b6hd", "template_h51xoxl", parms).then(alert("Email sent!"))
+
 }
